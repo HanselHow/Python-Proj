508a29014baa300d176d6c2ebb9244086c082f8b
test37@CYNS.LOCAL <test37@CYNS.LOCAL>
2020-02-13 20:06:54 -0800

    Initial commit

---
+++ DA_DineshGrp.py
@@ -0,0 +1,36 @@
+import requests
+import scrapy
+import unittest
+
+url = 'http://172.18.58.238/snow/'
+r = requests.get(url)
+print("\t *", r.status_code)
+
+h = requests.get(url)
+for x in h.headers:
+ print("\t", x, ":", h.headers[x])
+
+
+headers = {
+    'User-Agent' : 'Mobile'
+}
+URLheader = ('http://172.18.58.238/headers.php')
+rh = requests.get(URLheader, headers=headers)
+print(rh.text)
+
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['http://172.18.58.238/snow/']
+    def parse(selfself, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield {
+                'Image Link' : x.xpath(newsel).extract_first(),
+            }
+
+class Test_NewSpider(unittest.TestCase):
+    def test_spider(selfself):
+        NewSpider()
+
+if __name__ == '__main__': unittest.main()
